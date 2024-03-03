05b841aeb32bbe772a1d4d09f1797087f5244448
Rupesh <rupeshkhatri9816@gmail.com>
2024-03-04 01:15:00 +0545

    some changes

--- app/page.js
+++ app/page.js
@@ -3,11 +3,11 @@
 export default function Home() {
   return (
    <>
-      <div className="container bg-gray-100 ">
-      <div className='m-auto flex justify-center items-center'>
-                <h1 className='text-3xl font-bold text-center '>This is the home Page</h1>
+     
+     <div className=' flex justify-center items-center min-h-screen'>
+                <h1 className='text-3xl font-bold text-center '>This is home Page</h1>
             </div>
-      </div>
+     
    </>
   );
 }
